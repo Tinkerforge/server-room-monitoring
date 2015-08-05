a517c008ab1319ca954019531b64121668471fa0
Matthias Bolte <matthias@tinkerforge.com>
2015-08-05 16:20:18 +0200

    Cleanup Nagios scripts and add humidity support

--- nagios_icinga/check_tf_temp.py
+++ nagios_icinga/check_tf_temp.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 # based on Wiki project:
 # http://www.tinkerunity.org/wiki/index.php/EN/Projects/IT_Infrastructure_Monitoring_-_Nagios_Plugin
- 
+
 OK = 0
 WARNING = 1
 CRITICAL = 2
@@ -10,80 +10,79 @@ UNKNOWN = 3
 
 TYPE_PTC = "ptc"
 TYPE_TEMPERATURE = "temp"
- 
+
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_temperature import Temperature
 from tinkerforge.bricklet_ptc import PTC
 import argparse
 import sys
- 
+
 class CheckTFTemperature(object):
     def __init__(self, host='localhost', port=4223):
         self.host = host
         self.port = port
         self.ipcon = IPConnection()
 
- 
     def connect(self, type, uid):
         self.ipcon.connect(self.host, self.port)
         self.connected_type = type
 
         if self.connected_type == TYPE_PTC:
-            ptc = PTC(uid,self.ipcon)
+            ptc = PTC(uid, self.ipcon)
             self.func = ptc.get_temperature
         elif self.connected_type == TYPE_TEMPERATURE:
-            temperature = Temperature(uid,self.ipcon)
+            temperature = Temperature(uid, self.ipcon)
             self.func = temperature.get_temperature
- 
+
     def disconnect(self):
         self.ipcon.disconnect()
- 
+
     def read_temperature(self):
         return self.func()/100.0
- 
+
     def read(self, warning, critical, mode='none', warning2=0, critical2=0):
         temp = self.read_temperature()
 
         if mode == 'none':
-            print "Temperature %s° " % temp
+            print "temperature %s °C" % temp
         else:
-        
             if mode == 'low':
                 warning2 = warning
                 critical2 = critical
 
             if temp >= critical and (mode == 'high' or mode == 'range'):
-                print "CRITICAL : Temperature too high %s°C " % temp
+                print "CRITICAL : temperature too high %s °C" % temp
                 return CRITICAL
             elif temp >= warning and (mode == 'high' or mode == 'range'):
-                print "WARNING : Temperature is high %s°C " % temp
+                print "WARNING : temperature is high %s °C" % temp
                 return WARNING
             elif temp <= critical2 and (mode == 'low' or mode == 'range'):
-                print "CRITICAL : Temperature too low %s°C " % temp
+                print "CRITICAL : temperature too low %s °C" % temp
                 return CRITICAL
             elif temp <= warning2 and (mode == 'low' or mode == 'range'):
-                print "WARNING : temperature is low %s°C " % temp
+                print "WARNING : temperature is low %s °C" % temp
                 return WARNING
-            elif (temp < warning and mode == 'high') or (temp > warning2 and mode == 'low') or (temp < warning and temp > warning2 and mode == 'range'):
+            elif (temp < warning and mode == 'high') or \
+                 (temp > warning2 and mode == 'low') or \
+                 (temp < warning and temp > warning2 and mode == 'range'):
                 print "OK : %s°C " % temp
                 return OK
             else:
-                print "UNKOWN: Can't read temperature"
+                print "UNKNOWN : can't read temperature"
                 return UNKNOWN
- 
+
 if __name__ == "__main__":
- 
     parse = argparse.ArgumentParser()
     parse.add_argument("-u", "--uid", help="UID from Temperature Bricklet", required=True)
     parse.add_argument("-t", "--type", help="Type: temp = Temperature Bricklet, ptc = PTC Bricklet", type=str, choices=[TYPE_TEMPERATURE, TYPE_PTC], required=True)
     parse.add_argument("-H", "--host", help="Host Server (default=localhost)", default='localhost')
     parse.add_argument("-P", "--port", help="Port (default=4223)", type=int, default=4223)
-    parse.add_argument("-m", "--modus", help="Modus: none (default, only print temperature), high, low or range",type=str, choices=['none', 'high','low','range'], default='none')
-    parse.add_argument("-w", "--warning", help="Warning temperature level (temperatures above this level will trigger a warning message in high mode, temperature below this level will trigger a warning message in low mode)", required=False,type=float)
-    parse.add_argument("-c", "--critical", help="Critical temperature level (temperatures above this level will trigger a critical message in high mode, temperature below this level will trigger a critical message in low mode)", required=False,type=float)
+    parse.add_argument("-m", "--modus", help="Modus: none (default, only print temperature), high, low or range", type=str, choices=['none', 'high','low','range'], default='none')
+    parse.add_argument("-w", "--warning", help="Warning temperature level (temperatures above this level will trigger a warning message in high mode, temperature below this level will trigger a warning message in low mode)", required=False, type=float)
+    parse.add_argument("-c", "--critical", help="Critical temperature level (temperatures above this level will trigger a critical message in high mode, temperature below this level will trigger a critical message in low mode)", required=False, type=float)
     parse.add_argument("-w2", "--warning2", help="Warning temperature level (temperatures below this level will trigger a warning message in range mode)", type=float)
     parse.add_argument("-c2", "--critical2", help="Critical temperature level (temperatures below this level will trigger a critical message in range mode)", type=float)
- 
+
     args = parse.parse_args()
 
     tf = CheckTFTemperature(args.host, args.port)
--- nagios_icinga/check_tf_temp_ext.py
+++ nagios_icinga/check_tf_temp_ext.py
@@ -2,7 +2,7 @@
 # -*- coding: utf8 -*-
 # based on Wiki project:
 # http://www.tinkerunity.org/wiki/index.php/EN/Projects/IT_Infrastructure_Monitoring_-_Nagios_Plugin
- 
+
 OK = 0
 WARNING = 1
 CRITICAL = 2
@@ -12,40 +12,53 @@ NO_MOTION_DETECTED = 0
 
 TYPE_PTC = "ptc"
 TYPE_TEMPERATURE = "temp"
+TYPE_HUMIDITY = "humidity"
 TYPE_MOTION_DETECTOR = "motion_detector"
 TYPE_SEGMENT_DISPLAY_4X7 = "segment_display_4x7"
- 
+
 from tinkerforge.ip_connection import IPConnection
 from tinkerforge.bricklet_temperature import Temperature
 from tinkerforge.bricklet_ptc import PTC
+from tinkerforge.bricklet_humidity import Humidity
 from tinkerforge.bricklet_motion_detector import MotionDetector
 from tinkerforge.bricklet_segment_display_4x7 import SegmentDisplay4x7
 import argparse
 import sys
- 
+
 class CheckTFTemperature(object):
     def __init__(self, host='localhost', port=4223):
         self.host = host
         self.port = port
         self.ipcon = IPConnection()
- 
+        self.name = 'unknown'
+        self.unit = 'unknown'
+
     def connect(self, type, uid):
         self.ipcon.connect(self.host, self.port)
         self.connected_type = type
 
         if self.connected_type == TYPE_PTC:
-            ptc = PTC(uid,self.ipcon)
+            ptc = PTC(uid, self.ipcon)
             self.func = ptc.get_temperature
+            self.name = 'temperature'
+            self.unit = '°C'
         elif self.connected_type == TYPE_TEMPERATURE:
-            temperature = Temperature(uid,self.ipcon)
+            temperature = Temperature(uid, self.ipcon)
             self.func = temperature.get_temperature
+            self.name = 'temperature'
+            self.unit = '°C'
+        elif self.connected_type == TYPE_HUMIDITY:
+            humidity = Humidity(uid, self.ipcon)
+            self.func = humidity.get_humidity
+            self.name = 'humidity'
+            self.unit = '%RH'
         elif self.connected_type == TYPE_MOTION_DETECTOR:
-            md = MotionDetector(uid,self.ipcon)
+            md = MotionDetector(uid, self.ipcon)
             self.func = md.get_motion_detected
         elif self.connected_type == TYPE_SEGMENT_DISPLAY_4X7:
-            display = SegmentDisplay4x7(uid,self.ipcon)
+            display = SegmentDisplay4x7(uid, self.ipcon)
             self.func = display.set_segments
- 
+
     def disconnect(self):
         self.ipcon.disconnect()
 
@@ -55,74 +68,77 @@ class CheckTFTemperature(object):
         else:
             self.func((0, 0, 0, 0), 8, False)
 
- 
     def read_sensor(self):
-        if self.connected_type == TYPE_MOTION_DETECTOR:
+        if self.connected_type == TYPE_HUMIDITY:
+            return self.func()/10.0
+        elif self.connected_type == TYPE_MOTION_DETECTOR:
             return self.func()
         else: # Temperature, PTC
             return self.func()/100.0
- 
-    def read(self, warning, critical, mode='none', warning2=0, critical2=0):
 
+    def read(self, warning, critical, mode='none', warning2=0, critical2=0):
         val = self.read_sensor()
 
         if self.connected_type == TYPE_MOTION_DETECTOR:
             if val == 0:
-                print "Motion Detected"
+                print "motion detected"
                 return MOTION_DETECTED
             else:
-                print "No Motion Detected"
+                print "no motion detected"
                 return NO_MOTION_DETECTED
         else:
-
             if mode == 'none':
-                print "Temperature %s° " % temp
+                print "%s %s %s" % (self.name, val, self.unit)
             else:
                 if mode == 'low':
                     warning2 = warning
                     critical2 = critical
 
                 if val >= critical and (mode == 'high' or mode == 'range'):
-                    print "CRITICAL : Temperature too high %s°C " % val
+                    print "CRITICAL : %s too high %s %s" % (self.name, val, self.unit)
                     return CRITICAL
                 elif val >= warning and (mode == 'high' or mode == 'range'):
-                    print "WARNING : Temperature is high %s°C " % val
+                    print "WARNING : %s is high %s %s" % (self.name, val, self.unit)
                     return WARNING
                 elif val <= critical2 and (mode == 'low' or mode == 'range'):
-                    print "CRITICAL : Temperature too low %s°C " % val
+                    print "CRITICAL : %s too low %s %s" % (self.name, val, self.unit)
                     return CRITICAL
                 elif val <= warning2 and (mode == 'low' or mode == 'range'):
-                    print "WARNING : temperature is low %s°C " % val
+                    print "WARNING : %s is low %s %s" % (self.name, val, self.unit)
                     return WARNING
-                elif (val < warning and mode == 'high') or (val > warning2 and mode == 'low') or (val < warning and val > warning2 and mode == 'range'):
-                    print "OK : %s°C " % val
+                elif (val < warning and mode == 'high') or \
+                     (val > warning2 and mode == 'low') or \
+                     (val < warning and val > warning2 and mode == 'range'):
+                    print "OK : %s %s" % (val, self.unit)
                     return OK
                 else:
-                    print "UNKOWN: Can't read temperature"
+                    print "UNKNOWN : can't read %s" % self.name
                     return UNKNOWN
 
 if __name__ == "__main__":
- 
     parse = argparse.ArgumentParser()
     parse.add_argument("-u", "--uid", help="UID from Bricklet", required=True)
-    parse.add_argument("-t", "--type", help="Choose fitting type for your Bricklet", type=str, choices=[TYPE_TEMPERATURE, TYPE_PTC, TYPE_MOTION_DETECTOR, TYPE_SEGMENT_DISPLAY_4X7], required=True)
+    parse.add_argument("-t", "--type", help="Choose fitting type for your Bricklet", type=str, choices=[TYPE_TEMPERATURE, TYPE_PTC, TYPE_HUMIDITY, TYPE_MOTION_DETECTOR, TYPE_SEGMENT_DISPLAY_4X7], required=True)
     parse.add_argument("-H", "--host", help="Host Server (default=localhost)", default='localhost')
     parse.add_argument("-P", "--port", help="Port (default=4223)", type=int, default=4223)
-    parse.add_argument("-m", "--modus", help="Modus: none (default, only print temperature), high, low or range",type=str, choices=['none', 'high','low','range'], default='none')
-    parse.add_argument("-w", "--warning", help="Warning temperature level (temperatures above this level will trigger a warning message in high mode, temperature below this level will trigger a warning message in low mode)", required=False,type=float)
-    parse.add_argument("-c", "--critical", help="Critical temperature level (temperatures above this level will trigger a critical message in high mode, temperature below this level will trigger a critical message in low mode)", required=False,type=float)
-    parse.add_argument("-w2", "--warning2", help="Warning temperature level (temperatures below this level will trigger a warning message in range mode)", type=float)
-    parse.add_argument("-c2", "--critical2", help="Critical temperature level (temperatures below this level will trigger a critical message in range mode)", type=float)
+    parse.add_argument("-m", "--modus", help="Modus: none (default, only print value), high, low or range", type=str, choices=['none', 'high','low','range'], default='none')
+    parse.add_argument("-w", "--warning", help="Warning value level (values above this level will trigger a warning message in high mode, values below this level will trigger a warning message in low mode)", required=False, type=float)
+    parse.add_argument("-c", "--critical", help="Critical value level (values above this level will trigger a critical message in high mode, values below this level will trigger a critical message in low mode)", required=False, type=float)
+    parse.add_argument("-w2", "--warning2", help="Warning value level (values below this level will trigger a warning message in range mode)", type=float)
+    parse.add_argument("-c2", "--critical2", help="Critical value level (values below this level will trigger a critical message in range mode)", type=float)
     parse.add_argument("-e", "--error", help="Set Error Message on 4x7 Segment On/Off", choices=['true', 'false'], type=str, default='true')
- 
+
     args = parse.parse_args()
 
     tf = CheckTFTemperature(args.host, args.port)
     tf.connect(args.type, args.uid)
+
     if args.type == TYPE_SEGMENT_DISPLAY_4X7:
         tf.error(args.error)
         exit_code = 0
     else:
-        exit_code = tf.read(args.warning, args.critical, args.modus, args.warning2, args.critical2)
+        exit_code = tf.read(args.warning, args.critical, args.modus,
+                            args.warning2, args.critical2)
+
     tf.disconnect()
     sys.exit(exit_code)
